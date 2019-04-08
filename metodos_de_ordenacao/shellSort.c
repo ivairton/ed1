770bdde2d1939ad2977a97dd1c7f9fe401587899
ivairton <40799466+ivairton@users.noreply.github.com>
2019-04-08 09:18:27 -0300

    Create shellSort.c

---
+++ metodos_de_ordenacao/shellSort.c
@@ -0,0 +1,133 @@
+/**
+ * UFMT - Ciencia da Computacao
+ * ED1 - Ivairton
+ *
+ * Implementacao da estrutura lista e método de ordenação Shell Sort
+ *
+ */
+
+//Inclusao de bibliotecas
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+//Constante
+#define N 100
+
+//Definicao de tipo
+struct estruturaLista {
+    int lista[N];
+    int qtd;
+};
+typedef struct estruturaLista tipoLista;
+
+//Prototipos
+void insereFimLista (tipoLista*, int);
+int removePosicaoLista (tipoLista*, int);
+void imprimeLista (tipoLista);
+//Metodos de ordenação
+void shellSort(tipoLista*);
+
+//Funcao principal
+int main(int argc, char *argv[]){
+    tipoLista lista;
+    lista.qtd = 0;
+
+    //Geracao de numeros pseudoaleatorios
+    time_t tm; //Usado na geração da semente
+    srand( (unsigned) time(&tm) );
+
+    insereFimLista(&lista, rand()%20);
+    insereFimLista(&lista, rand()%20);
+    insereFimLista(&lista, rand()%20);
+    insereFimLista(&lista, rand()%20);
+    insereFimLista(&lista, rand()%20);
+    insereFimLista(&lista, rand()%20);
+    insereFimLista(&lista, rand()%20);
+    insereFimLista(&lista, rand()%20);
+    insereFimLista(&lista, rand()%20);
+    
+    imprimeLista(lista);
+
+    shellSort(&lista);
+    
+    imprimeLista(lista);
+
+    return 1;
+}
+
+//Funcao para insercao de valor no fim da lisat
+void insereFimLista (tipoLista* lst, int vl) {
+    if ( lst->qtd < N ) {
+        lst->lista[lst->qtd++] = vl;
+    }
+}
+
+//Funcao que remove um valor em posicao especifica da lista
+int removePosicaoLista (tipoLista* lst, int pos) {
+    int i, vl;
+
+    if ( (lst->qtd > 0) && (pos < lst->qtd) && (pos >= 0) ) {
+        vl = lst->lista[pos];
+        i = pos;
+        while ( i < lst->qtd-2 ) {
+            lst->lista[i] = lst->lista[i+1];
+            i++;
+        }
+        lst->qtd--;
+        return vl;
+    } else {
+        return -1;
+    }
+
+}
+
+//Funcao que imprime a estrutura
+void imprimeLista (tipoLista lst) {
+    int i;
+    printf("Lista: ");
+    for (i=0; i<lst.qtd; i++) {
+        printf("[%d]", lst.lista[i]);
+    }
+    printf("(Qtd = %d)\n", lst.qtd);
+}
+
+//Ordenacao Shell Sort
+void shellSort(tipoLista* lst) {
+    int i, salto, aux, pos;
+    //Calculo do gap
+    salto = (int) lst->qtd / 2;
+
+    //Executa passagens com saltos maiores que 1
+    while (salto > 1) {
+        printf("Passagem com gap = %d;\t", salto);
+        i = 0;
+        while ( (i+salto) < lst->qtd) {
+            if (lst->lista[i] > lst->lista[i+salto]) {
+                aux = lst->lista[i];
+                lst->lista[i] = lst->lista[i+salto];
+                lst->lista[i+salto] = aux;
+            }
+            i++;
+        }
+        //Atualiza gap
+        salto = salto / 2;
+
+        imprimeLista(*lst);
+    }
+    //Ultima passada, com gap = 1
+    i = 0;
+    while (i < lst->qtd-1) {
+        if (lst->lista[i] > lst->lista[i+1]) {
+            //faz a troca e verifica com os anteriores
+            pos = i+1;
+            while ( (pos > 0) && (lst->lista[pos-1] > lst->lista[pos]) ) {
+                aux = lst->lista[pos];
+                lst->lista[pos] = lst->lista[pos-1];
+                lst->lista[pos-1] = aux;
+                pos--;
+            }
+        }
+        i++;
+    }
+}
